35cb3b0aa8e3d8bdadd49b31e9e7ccd0f72ef856
Adson Cardoso <adson.cardosocic@gmail.com>
2025-10-01 17:59:26 -0300

    style: remove unnecessary comments

--- src/schema.ts
+++ src/schema.ts
@@ -1,18 +1,3 @@
-/*
-Requirements:
-GraphQL API Implementation:
-Create a simple GraphQL API with the following schema:
-
-Query type with:
-getUser(id: ID!): User
-listUsers(limit: Int): [User!]!
-
-User type with fields:
-id: ID!
-name: String!
-email: String!
-age: Int */
-
 import dist = require("apollo-server");
 
 export const typeDefs = dist.gql `
